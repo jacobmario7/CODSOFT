da81945c4b34c32cfa11b9388bcb38b067bc20e4
jacobmario7 <160170771+jacobmario7@users.noreply.github.com>
2024-04-22 00:14:27 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,3 +1,4 @@
+//Importing libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,19 +6,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
+//Loading & checking the dataset
 iris=pd.read_csv('C:\\Users\\Jacob Mario Leonard\\Downloads\\IRIS.csv')
 df=pd.DataFrame(iris)
 print(df.head())
 
+//Data processing
 x = df.drop('species', axis=1)
 y = df['species']
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
+//Training the model
 model = SVC(kernel='linear')
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
+//Results
 print(classification_report(y_test, y_pred))
 
 print(confusion_matrix(y_test, y_pred))
